10a3d9801c52ac970ee1af3443525c019714c70d
LeeJunhui <84416097@qq.com>
2017-09-28 11:58:49 +0800

    init commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstWatchApp
+//
+//  Created by leejunhui on 28/09/2017.
+//  Copyright © 2017 leejunhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstWatchApp
+//
+//  Created by leejunhui on 28/09/2017.
+//  Copyright © 2017 leejunhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
